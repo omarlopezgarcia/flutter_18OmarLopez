72ab2ad0a5d84f8defb8fc7d23a7a9f3e6e44e45
OmarLopez <omar.lopez@vallegrande.edu.pe>
2023-08-28 15:17:55 -0500

    Hola Mundo - Prueba v0.1

--- lib/main.dart
+++ lib/main.dart
@@ -106,7 +106,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'Hola mundo cuantas veces:',
             ),
             Text(
               '$_counter',

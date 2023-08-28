a8b1303c0df3e6d50237aaafdfd32fda8edc1947
OmarLopez <omar.lopez@vallegrande.edu.pe>
2023-08-28 18:27:05 -0500

    Hola mundo - Prueba v0.2

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'BiblioTech'),
     );
   }
 }
@@ -106,7 +106,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'Hola mundo cuantas veces:',
+              'Entrando a BiblioTech cuantas veces:',
             ),
             Text(
               '$_counter',
